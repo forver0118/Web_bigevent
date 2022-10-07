205a4e03f42f9c89a1f753922ba27398e9880f9b
kingJ <Jinghaoo0118@163.com>
2022-10-07 10:09:34 +0800

    完成个人中心的开发

--- assets/js/user_info.js
+++ assets/js/user_info.js
@@ -12,7 +12,6 @@ $(function(){
 
   initUserInfo()
   // 初始化用户的基本信息
-
   function initUserInfo(){
     $.ajax({
       method:'GET',
@@ -27,4 +26,31 @@ $(function(){
       }
     })
   }
+
+  // 重置表单的数据
+  $('#btnReset').on('click',function(e){
+    // 阻止表单的默认重置行为
+    e.preventDefault()
+    initUserInfo()
+  })
+
+  // 监听表单的提交事件
+  $('.layui-form').on('submit',function(e){
+    // 阻止表单的默认提交行为
+    e.preventDefault()
+    // 发起ajax数据请求
+    $.ajax({
+      method:'POST',
+      url:'/my/userinfo',
+      data:$(this).serialize(),
+      success:function(res){
+        if(res.status!==0){
+          return layer.msg('更新用户信息失败！')
+        }
+        layer.msg('更新用户信息成功！')
+        // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+        window.parent.getUserInfo()
+      }
+    })
+  })
 })
--- assets/js/user_pwd.js
+++ assets/js/user_pwd.js
@@ -4,12 +4,12 @@ $(function(){
   form.verify({
     pass:[/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
     samePwd:function(value){
-      if(value===$('[name=old_pwd]').val()){
+      if(value===$('[name=oldPwd]').val()){
         return '新旧密码不能一样'
       }
     },
     rePwd:function(value){
-      if(value!==$('[name=new_pwd]').val()){
+      if(value!==$('[name=newPwd]').val()){
         return '两次输入的密码不一致'
       }
     }   
@@ -22,10 +22,13 @@ $(function(){
     $.ajax({
       method:'PATCH',
       url:'/my/updatepwd',
-      data:form.val('pwdForm'),
-      success(res){
-        if(res.code!==0)return layer.msg('修改密码失败')
-        layer.msg('修改密码成功')
+      data:$(this).serialize(),
+      success:function(res){
+        if(res.status!==0){
+          return layui.layer.msg('更新密码失败！')
+        }
+        layui.layer.msg('更新密码成功！')
+        // 重置表单
         $('.layui-form')[0].reset()
       }
     })
